1bcdfca4d176dba62e61926ddd3231431bab3006
Hrutuja182 <hrutujadiwane01@gmail.com>
2025-07-21 22:23:07 +0200

    new changes for refatoring

--- tests/buttonVisibilityTest.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function testButtonVisibility(driver) {
-  try {
-    const btns = await driver.findElements({ css: 'button, input[type="button"]' });
-    console.log(`🔘 Found ${btns.length} buttons`);
-  } catch {
-    console.log('ℹ️ No buttons found');
-  }
-};
--- tests/responsiveUITest.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function testResponsiveUI(driver) {
-  const sizes = [[375, 667], [768, 1024], [1440, 900]];
-  console.log('📱 Testing responsiveness...');
-  for (const [w, h] of sizes) {
-    await driver.manage().window().setRect({ width: w, height: h });
-    await driver.sleep(500);
-    console.log(`✅ Viewport ${w}x${h} tested`);
-  }
-};
